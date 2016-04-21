45b7b7d56d311075526cf2307e6cd25d203f3ff9
Simon Johansson <amp@ampleyfly.se>
2016-04-21 19:05:32 +0200

    Drawing white hello world works

--- display.c
+++ display.c
@@ -6,6 +6,7 @@ u8g_t u8g;
 
 void draw(void)
 {
+    u8g_SetColorIndex(&u8g, 255);
     u8g_SetFont(&u8g, u8g_font_6x10);
     u8g_DrawStr(&u8g, 0, 15, "Hello World!");
 }
@@ -16,14 +17,14 @@ int main(void)
     //CLKPR = 0x80;
     //CLKPR = 0x00;
     /*
-     * CS: PORTB, Bit 2 --> PN(1,2)
-     * A0: PORTB, Bit 1 --> PN(1,1)
-     * SCK: PORTB, Bit 5 --> PN(1,5)
-     * MOSI: PORTB, Bit 3 --> PN(1,3)
-     * RST: PORTB, Bit 0 --> PN(1,0)
+     * RST: PORTD, Bit 2 --> PN(3,2)
+     * A0: PORTD, Bit 3 --> PN(3,3)
+     * CS: PORTD, Bit 4 --> PN(3,4)
+     * MOSI: PORTD, Bit 5 --> PN(3,5)
+     * SCK: PORTD, Bit 6 --> PN(3,6)
      */
 
-    u8g_InitSPI(&u8g, &u8g_dev_ili9225_176x220_sw_spi, PN(1, 5), PN(1, 3), PN(1, 2), PN(1, 1), PN(1, 0));
+    u8g_InitSPI(&u8g, &u8g_dev_ili9225_176x220_sw_spi, PN(3, 6), PN(3, 5), PN(3, 4), PN(3, 3), PN(3, 2));
 
     for (;;)
     {
